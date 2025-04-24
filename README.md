b58160aa4586f92da90cf3af9ad8d9e9bfaa8b19
Jin Igarashi <igarashi.jin@water-gis.com>
2025-04-24 17:36:50 +0900

    fix: migrated JupyterHub settings from geo-cb-surge

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # rapida-jupyter
 Jupyter Hub for RAPIDA tool
+
+## Development
+
+This repository uses Docker to launch Jupyter Hub locally. Makefile provides you necessary commands to setup Jupyter Hub.
+
+- Build Docker image
+
+```shell
+make build
+```
+
+If you would like to build image for production, execute the below command
+
+```shell
+PRODUCTION=true make build
+```
+
+- Launch Jupyter Hub
+
+```shell
+make up
+```
+
+You can access to JupyterHub through `http:localhost:8100` in your browser. 
+
+- set users
+
+Authenticate users can be set through `JUPYTER_USERS` variable at `.env` file.
+
+```
+cp .env.example .env
+vi .env
+```
+
+JUPYTER_USERS can have multiple users (username:password) for authentication
+
+```shell
+JUPYTER_USERS=docker:docker user:user
+```
+
+folder structure in the container will be as follows:
+
+- /data - it will be mounted to fileshare in Azure. All files under this folder will be kept
+  - /data/{username} - users can explore all users file, a user has no permission to edit other users' folder.
+- /home/{username} - user home folder. This data will be lost when the server is restarted.
+
+### destroy docker container
+
+```shell
+make down
+```
+
+### enter to Docker container
+
+```shell
+make shell
+```
+
+Note. if you use `make shell`, entrypoint.sh is not executed. That means you have to install RAPIDA tool manually like below.
+
+```shell
+pipenv run pip install git+https://github.com/UNDP-Data/geo-cb-surge
+```
---
+++ entrypoint.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# install rapida tool and dependencies
+pipenv run pip install playwright
+pipenv run playwright install chromium --with-deps
+pipenv run pip install git+https://github.com/UNDP-Data/geo-cb-surge
+
+# Create multiple users from environment variable SSH_USERS
+# Format: JUPYTER_USERS="user1:password1 user2:password2 user3:password3"
+if [ ! -z "$JUPYTER_USERS" ]; then
+    for user_info in $JUPYTER_USERS; do
+        IFS=':' read -r username password <<< "$user_info"
+        if [ ! -z "$username" ] && [ ! -z "$password" ]; then
+            /app/create_user.sh "$username" "$password"
+
+            echo "Creating Jupyter user $username profile directories..."
+
+            # Ensure the home directory exists first
+            if [ ! -d "/home/$username" ]; then
+                echo "Home directory for $username does not exist. Creating..."
+                mkdir -p /home/$username
+                chown $username:$username /home/$username
+            fi
+
+            # Create the full path for .ipython/profile_default/startup
+            mkdir -p /home/$username/.ipython/profile_default/startup
+            chown -R $username:$username /home/$username/.ipython
+
+            # Now copy the cell hook
+            cp /app/cbsurge/az/cell_hook.py /home/$username/.ipython/profile_default/startup/cell_hook.py
+            chown $username:$username /home/$username/.ipython/profile_default/startup/cell_hook.py
+
+        else
+            echo "Invalid user format: $user_info"
+        fi
+    done
+fi
+
+# Determine the port based on the PRODUCTION environment variable
+if [ "$PRODUCTION" = "true" ]; then
+    JUPYTER_PORT=80
+else
+    JUPYTER_PORT=8000
+fi
+
+# Start JupyterLab in the foreground (so the container keeps running)
+pipenv run jupyterhub -f jupyterhub_config.py --port=$JUPYTER_PORT
