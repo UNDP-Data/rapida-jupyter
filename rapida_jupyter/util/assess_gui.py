e486066f150a1691ca3acaec5fdf51a56ac42dfb
Joseph Thuha <josephthuha@gmail.com>
2025-05-09 10:57:19 +0300

    Fix/improve display geodata (#11)
    * feat: add argument to pass geopackage direct to display_data function
    
    * feat: add select all button for assessment gui

--- rapida_jupyter/util/assess_gui.py
+++ rapida_jupyter/util/assess_gui.py
@@ -1,3 +1,5 @@
+import sys
+
 import ipywidgets as widgets
 from IPython.display import display
 from rapida.cli.assess import getComponents
@@ -24,25 +26,52 @@ def assessment_gui(project_path):
     with Session() as session:
         components = getComponents()
         tab_panels = {}
+        tab_checkboxes = {}
+        tab_select_all_boxes = []
         tab_children = []
 
         for component in components:
             variables = session.get_variables(component=component)
             checkboxes = [widgets.Checkbox(value=False, description=session.get_variable_title(component=component, variable=v)) for v in variables]
 
+            tab_checkboxes[component] = checkboxes
+
+            select_all = widgets.Checkbox(value=False, description="Select All in Tab")
+
+            def create_tab_toggle(select_all_box, checkbox_list):
+                def on_toggle(change):
+                    if change["name"] == "value" and change["new"] != change["old"]:
+                        for cb in checkbox_list:
+                            cb.unobserve_all()  # prevent cascading
+                            cb.value = change["new"]
+                            cb.observe(update_tab_select_all, names='value')
+                return on_toggle
+
+            def update_tab_select_all(change):
+                # Check if all are selected/deselected to reflect in the main toggle
+                all_checked = all(cb.value for cb in checkbox_list)
+                any_unchecked = any(not cb.value for cb in checkbox_list)
+                select_all.unobserve_all()
+                select_all.value = all_checked
+                select_all.observe(create_tab_toggle(select_all, checkbox_list), names='value')
+
+            checkbox_list = checkboxes
+            select_all.observe(create_tab_toggle(select_all, checkbox_list), names='value')
+
+            for cb in checkbox_list:
+                cb.observe(update_tab_select_all, names='value')
+
+            tab_select_all_boxes.append(select_all)
+
             if len(checkboxes) > 5:
                 cols = 2 if len(checkboxes) <= 10 else 3
-                rows = (len(checkboxes) + cols - 1) // cols
                 grid = widgets.GridBox(
                     children=checkboxes,
-                    layout=widgets.Layout(
-                        grid_template_columns=' '.join(['auto'] * cols),
-                        grid_gap="5px 5px"
-                    )
+                    layout=widgets.Layout(grid_template_columns=' '.join(['auto'] * cols), grid_gap="5px 5px")
                 )
-                var_container = grid
+                var_container = widgets.VBox([select_all, grid])
             else:
-                var_container = widgets.VBox(checkboxes)
+                var_container = widgets.VBox([select_all] + checkboxes)
 
             tab_panels[component] = var_container
             tab_children.append(var_container)
@@ -51,36 +80,57 @@ def assessment_gui(project_path):
         for i, component in enumerate(components):
             tabs.set_title(i, component)
 
-        run_assessment_btn = widgets.Button(
-            description="Run Assessment",
-            button_style='info',
-            tooltip='Run Assessment'
-        )
+        global_select_all = widgets.Button(description="Select All", button_style='success')
+        global_deselect_all = widgets.Button(description="Deselect All", button_style='warning')
 
+        def global_toggle(value):
+            for component, cbs in tab_checkboxes.items():
+                for cb in cbs:
+                    cb.unobserve_all()
+                    cb.value = value
+                    cb.observe(update_tab_select_all, names='value')
+                sel_all = tab_select_all_boxes[components.index(component)]
+                sel_all.unobserve_all()
+                sel_all.value = value
+                sel_all.observe(create_tab_toggle(sel_all, cbs), names='value')
+
+        global_select_all.on_click(lambda btn: global_toggle(True))
+        global_deselect_all.on_click(lambda btn: global_toggle(False))
+
+        run_assessment_btn = widgets.Button(description="Run Assessment", button_style='info')
         output = widgets.Output()
 
         def run_assessment(btn):
             btn.description = "Running..."
             btn.disabled = True
+            global_select_all.disabled = True
+            global_deselect_all.disabled = True
             with output:
                 output.clear_output()
-                for component, var_box in tab_panels.items():
+                for component, container in tab_panels.items():
+                    widgets_in_box = container.children
+                    if isinstance(widgets_in_box[1], widgets.GridBox):
+                        cbs = widgets_in_box[1].children
+                    else:
+                        cbs = widgets_in_box[1:]  # after select_all
                     selected_vars = [
-                        session.get_var_id_from_title(cb.description) for cb in var_box.children
-                        if isinstance(cb, widgets.Checkbox) and cb.value
+                        session.get_var_id_from_title(cb.description)
+                        for cb in cbs if isinstance(cb, widgets.Checkbox) and cb.value
                     ]
-                    if isinstance(var_box, widgets.GridBox):
-                        selected_vars = [
-                            session.get_var_id_from_title(cb.description) for cb in var_box.children if cb.value
-                        ]
                     for var in selected_vars:
                         cmd_args = f"-c {component} -v {var}"
+                        btn.description = f"Running... {var}"
                         run_cmd("assess", cmd_args)
-                        print(cmd_args)
             btn.description = "Run Assessment"
             btn.disabled = False
 
-        title_widget = widgets.Label(value="Run Assessment")
+
         run_assessment_btn.on_click(run_assessment)
 
-        display(title_widget, tabs, run_assessment_btn, output)
+
+        display(
+            widgets.Label(value="Run Assessment"),
+            tabs,
+            widgets.HBox([global_select_all, global_deselect_all, run_assessment_btn]),
+            output
+        )
